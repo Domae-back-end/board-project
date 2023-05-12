d0343d0123865c92f7bb291a76f27643b2724d36
Domae-back-end <ty_ty123@naver.com>
2023-05-12 18:38:08 +0900

    create: JpaRepository 테스트 코드 완성

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -1,6 +1,7 @@
 package com.fastcampus.projectboard.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
@@ -11,36 +12,30 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Getter
 @ToString
 @Table(indexes = {
-        @Index(columnList = "title"),
-        @Index(columnList = "hashtag"),
+        @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
 @EntityListeners(AuditingEntityListener.class)
 @Entity
-public class Article {
+public class ArticleComment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Setter
-    @Column(nullable = false)
-    private String title;
+    @ManyToOne(optional = false)
+    private Article article;
     @Setter
-    @Column(nullable = false, length = 10000)
+    @Column(nullable = false, length = 500)
     private String content;
-
-
-    @Setter
-    private String hashtag;
 
 
     @CreatedDate
@@ -59,35 +54,30 @@ public class Article {
     @Column(nullable = false, length = 100)
     private String modifiedBy;
 
-    @ToString.Exclude
-    @OrderBy("id")
-    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
-    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    protected Article() {
-    }
-
-    private Article(String title, String content, String hashtag) {
-        this.title = title;
-        this.content = content;
-        this.hashtag = hashtag;
-    }
-
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Article article)) return false;
-        return id != null && id.equals(article.id);
+        if (!(o instanceof ArticleComment that)) return false;
+        return id != null && id.equals(that.id);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article,content);
+    }
+
+
 
 
 }
